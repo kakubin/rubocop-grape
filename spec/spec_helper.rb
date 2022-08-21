362188fdd3c84b6aa30f9550435aff9b7d71ecc7
kakubin <wetsand.wfs@gmail.com>
2022-08-21 13:15:45 +0900

    raise error when warning detected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,9 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.raise_on_warning = true
+  config.raise_errors_for_deprecations!
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
