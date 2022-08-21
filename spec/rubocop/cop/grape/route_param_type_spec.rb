c8700584b7159579b0b528d0c2c489e454418b42
kakubin <wetsand.wfs@gmail.com>
2022-08-21 13:05:50 +0900

    fix route_param_type_spec.rb

--- spec/rubocop/cop/grape/route_param_type_spec.rb
+++ spec/rubocop/cop/grape/route_param_type_spec.rb
@@ -2,26 +2,38 @@
 
 RSpec.describe RuboCop::Cop::Grape::RouteParamType, :config do
   context 'if param type defined' do
-    it 'no offence reported' do
-      expect_no_offenses('route_param :id, type: Integer')
-    end
-  end
-
-  context 'if param type not defined' do
-    context 'with no any other options' do
-      it 'offence detected' do
-        expect_offense(<<~RUBY)
-          route_param :id do
-          ^^^^^^^^^^^^^^^
+    context 'without other options' do
+      it 'no offence reported' do
+        expect_no_offenses(<<~RUBY)
+          route_param :id, type: Integer
         RUBY
       end
     end
 
-    context 'with several options' do
+    context 'with other options' do
+      it 'no offence reported' do
+        expect_no_offenses(<<~RUBY)
+          route_param :id, type: Integer, desc: 'ID'
+        RUBY
+      end
+    end
+  end
+
+  context 'if param type not defined' do
+    context 'with other options' do
       it 'offence detected' do
         expect_offense(<<~RUBY)
-          route_param :id, desc: 'ID' do
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+          route_param :id
+          ^^^^^^^^^^^^^^^ Define Parameter Type
+        RUBY
+      end
+    end
+
+    context 'with other options' do
+      it 'offence detected' do
+        expect_offense(<<~RUBY)
+          route_param :id, desc: 'ID'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Define Parameter Type
         RUBY
       end
     end
